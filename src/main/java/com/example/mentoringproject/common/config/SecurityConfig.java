3e601e32385d9b92f23070f4aef7df96e19c814e
Jiny <49832261+sungjin-create@users.noreply.github.com>
2023-10-30 18:12:16 +0900

    Update SecurityConfig.java

--- src/main/java/com/example/mentoringproject/common/config/SecurityConfig.java
+++ src/main/java/com/example/mentoringproject/common/config/SecurityConfig.java
@@ -22,6 +22,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @Configuration
