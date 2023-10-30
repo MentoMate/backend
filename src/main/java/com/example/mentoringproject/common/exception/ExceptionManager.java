29680ec2f9f8b111277e924fc608102567b5d2d5
sungjin <sungjinny5@naver.com>
2023-10-30 21:29:46 +0900

    feature : exception 추가

---
+++ src/main/java/com/example/mentoringproject/common/exception/ExceptionManager.java
@@ -0,0 +1,15 @@
+package com.example.mentoringproject.common.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionManager {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> appExceptionHandler(AppException e) {
+        return ResponseEntity.status(e.getErrorCode()).body(e.getMessage());
+    }
+
+}
