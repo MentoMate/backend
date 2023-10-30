3db078574c7b33c08bc7117aae91f3fcdf13a032
Jiny <49832261+sungjin-create@users.noreply.github.com>
2023-10-30 18:14:16 +0900

    Update SecurityConfig.java

--- src/main/java/com/example/mentoringproject/common/config/SecurityConfig.java
+++ src/main/java/com/example/mentoringproject/common/config/SecurityConfig.java
@@ -25,6 +25,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 @Configuration
 @EnableWebSecurity
