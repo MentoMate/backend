7ecee1b64b20e10dd8c1095390c0ced81b0701b6
ECworld <128959426+ECworld@users.noreply.github.com>
2023-10-31 22:22:10 +0900

    feat : add mentorSearch by searchTye,sortBy,category (ElasticSearch)

---
+++ src/main/java/com/example/mentoringproject/ElasticSearch/mentor/controller/MentorSearchController.java
@@ -0,0 +1,41 @@
+package com.example.mentoringproject.ElasticSearch.mentor.controller;
+
+
+import com.example.mentoringproject.ElasticSearch.mentor.model.MentorSearchDto;
+import com.example.mentoringproject.ElasticSearch.mentor.service.MentorSearchService;
+import java.util.Comparator;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mentor")
+@RequiredArgsConstructor
+public class MentorSearchController {
+
+  private final MentorSearchService mentorSearchService;
+
+  @PostMapping("search")
+  public ResponseEntity<List<MentorSearchDto>> searchMentor(
+      @RequestParam(required = true) String searchCategory,
+      @RequestParam(required = true) String searchText,
+      @RequestParam(required = true) String sortBy) {
+
+    List<MentorSearchDto> mentorSearchDtoList = mentorSearchService.searchCategory(searchText,
+        searchCategory);
+
+    // 평점순, 최신순 정렬
+    if ("rating".equals(sortBy)) {
+      mentorSearchDtoList.sort(
+          Comparator.comparing(MentorSearchDto::getCareer).reversed()); // 평점 순으로 수정 필요
+    } else if ("latest".equals(sortBy)) {
+      mentorSearchDtoList.sort(Comparator.comparing(MentorSearchDto::getId).reversed());
+    }
+
+    return ResponseEntity.ok(mentorSearchDtoList);
+  }
+}
---
+++ src/main/java/com/example/mentoringproject/ElasticSearch/mentor/model/MentorSearchDto.java
@@ -0,0 +1,33 @@
+package com.example.mentoringproject.ElasticSearch.mentor.model;
+
+import com.example.mentoringproject.ElasticSearch.mentor.entity.MentorSearchDocumment;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class MentorSearchDto {
+
+  private Long id;
+  private String name;
+  private int career;
+  private String introduce;
+  private String mainCategory;
+  private String middleCategory;
+
+  public static MentorSearchDto fromDocument(MentorSearchDocumment mentorSearchDocumment) {
+    return MentorSearchDto.builder()
+        .id(mentorSearchDocumment.getId())
+        .name(mentorSearchDocumment.getName())
+        .career(mentorSearchDocumment.getCareer())
+        .introduce(mentorSearchDocumment.getIntroduce())
+        .mainCategory(mentorSearchDocumment.getMainCategory())
+        .middleCategory(mentorSearchDocumment.getMiddleCategory())
+        .build();
+  }
+
+}
