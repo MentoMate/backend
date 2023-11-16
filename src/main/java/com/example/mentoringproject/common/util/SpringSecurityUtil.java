fce131b93a8394fd042b8770717fad978c98414a
ECworld <128959426+ECworld@users.noreply.github.com>
2023-11-16 22:15:52 +0900

    add debug log

--- src/main/java/com/example/mentoringproject/common/util/SpringSecurityUtil.java
+++ src/main/java/com/example/mentoringproject/common/util/SpringSecurityUtil.java
@@ -1,15 +1,17 @@
 package com.example.mentoringproject.common.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
-
+@Slf4j
 @Component
 public class SpringSecurityUtil {
 
   public static String getLoginEmail() {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    log.debug("DEBUG :authentication :{}", authentication);
 
     if (authentication == null || authentication.getName() == null) {
       throw new RuntimeException("get authentication 오류");
