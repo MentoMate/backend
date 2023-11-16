a3783dd31909a067f75cc4b1a1a251dcab3b0f40
ECworld <128959426+ECworld@users.noreply.github.com>
2023-11-16 22:06:55 +0900

    test

--- src/main/java/com/example/mentoringproject/MentoringProjectApplication.java
+++ src/main/java/com/example/mentoringproject/MentoringProjectApplication.java
@@ -2,12 +2,13 @@ package com.example.mentoringproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
-@SpringBootApplication
 @EnableScheduling
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MentoringProjectApplication {
 
     public static void main(String[] args) {
