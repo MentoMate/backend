1d9363773db4a771c32ed11eb22eda7fddb01d5d
ECworld <128959426+ECworld@users.noreply.github.com>
2023-11-18 01:47:37 +0900

    improve : debug log

--- src/main/java/com/example/mentoringproject/chat/repository/PrivateChatRoomRepository.java
+++ src/main/java/com/example/mentoringproject/chat/repository/PrivateChatRoomRepository.java
@@ -2,6 +2,7 @@ package com.example.mentoringproject.chat.repository;
 
 import com.example.mentoringproject.chat.entity.PrivateChatRoom;
 import com.example.mentoringproject.user.user.entity.User;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,12 +11,8 @@ public interface PrivateChatRoomRepository extends JpaRepository<PrivateChatRoom
   boolean existsByUserIdAndMentoringId(Long userId, Long mentoringId);
   boolean existsByUser(User user);
   boolean existsByMentor(User user);
-  PrivateChatRoom findByUser(User user);
-  PrivateChatRoom findByMentor(User user);
-
-
-
-
+  PrivateChatRoom findFirstByUser(User user);
+  PrivateChatRoom findFirstByMentor(User user);
 
 }
 
