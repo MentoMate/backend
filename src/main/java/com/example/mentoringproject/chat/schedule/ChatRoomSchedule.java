45635813895849ee4c954620e34d9ff4c950dc0c
ECworld <128959426+ECworld@users.noreply.github.com>
2023-11-18 13:38:30 +0900

    improve : add Autowired anotation

--- src/main/java/com/example/mentoringproject/chat/schedule/ChatRoomSchedule.java
+++ src/main/java/com/example/mentoringproject/chat/schedule/ChatRoomSchedule.java
@@ -1,6 +1,7 @@
 package com.example.mentoringproject.chat.schedule;
 
 import com.example.mentoringproject.chat.service.ChatService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +10,7 @@ public class ChatRoomSchedule {
 
   private final ChatService chatService;
 
+  @Autowired
   public ChatRoomSchedule(ChatService chatService) {
     this.chatService = chatService;
   }
