fc4c1f9ccc2e45c8269839ca4d7fc76246c69169
ECworld <128959426+ECworld@users.noreply.github.com>
2023-11-20 02:25:41 +0900

    improve : change code structure

--- src/main/java/com/example/mentoringproject/chat/schedule/ChatRoomSchedule.java
+++ src/main/java/com/example/mentoringproject/chat/schedule/ChatRoomSchedule.java
@@ -1,19 +1,15 @@
 package com.example.mentoringproject.chat.schedule;
 
 import com.example.mentoringproject.chat.service.ChatService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class ChatRoomSchedule {
 
   private final ChatService chatService;
-
-  @Autowired
-  public ChatRoomSchedule(ChatService chatService) {
-    this.chatService = chatService;
-  }
 
   @Scheduled(cron = "0 0 0 * * ?")
   public void createChatRoomScheduled() {
